d4f456280ce5b87fc18095e8fb77b0c99f43ef83
UratileMei <uisleymei@gmail.com>
2026-01-08 19:50:03 +0200

    Engine Project created and Engine reference added to WPFUI

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; }  
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+        }
+    }
+}
