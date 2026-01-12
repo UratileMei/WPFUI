2ac118ddf4de69e979cd2345900a656fa1ed2d4d
UratileMei <uisleymei@gmail.com>
2026-01-12 16:12:06 +0200

    TODO

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : InotifyPropertyChanged
     {
         public string Name { get; set; }  
         public string CharacterClass { get; set; }
@@ -12,5 +13,14 @@ namespace Engine.Models
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
         public int Gold { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
     }
 }
