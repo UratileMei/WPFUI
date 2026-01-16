74d991aecf3e47f8ed7bca459e56d0115ac3315a
UratileMei <uisleymei@gmail.com>
2026-01-16 22:34:16 +0200

    INotifyPropertyChanged added, getters and setters updated from auto to manual.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Xml.Linq;
 
 namespace Engine.Models
 {
-    public class Player : InotifyPropertyChanged
+    public class Player : INotifyPropertyChanged
     {
-        public string Name { get; set; }  
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
-        public string Name
-        {
+        private int _experiencePoints;
+        private string _name;
+        private int _hitPoints;
+        private string _characterClass;
+        private int _level;
+        private int _gold;
+
+        public string Name { 
             get { return _name; }
-            set
-            {
-                _name = value;
+            set { _name = value;
                 OnPropertyChanged("Name");
             }
+        }
+        public string CharacterClass { 
+            get { return _characterClass; }
+            set { OnPropertyChanged("CharacterClass"); } }
+
+        public int HitPoints { get; set; }
+        public int ExperiencePoints {
+            get { 
+               return _experiencePoints; }
+            set
+            {
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
